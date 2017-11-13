90d89d463320001ec914a14a9a7676f9c2203a5e
Sergio R <s.rubio131@gmail.com>
2017-11-13 20:30:20 +0100

    Video 5. Two-way data binding

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,6 +1,29 @@
 // Identificador y array de módulos para registrarlos en la app (ngResource:consumir api rest)
-var app = angular.module("CursoAngularJS",["ngResource"]);
+var app = angular.module("CursoAngularJS",[]);
 
 app.controller("controller", function ($scope) {
-   $scope.nombre = "Sergio";
+   $scope.comentarios = [
+       {
+           username: "Pocholo",
+           comentario: "Buena onda wey"
+       },
+       {
+           username: "Paquito el Chocolatero",
+           comentario: "Tiro riro rari tirori tirori eh eh!"
+       }
+   ];
+
+   $scope.agregarComentario = function(username, comentario) {
+       if (username !== '' && comentario !== '') {
+           var comment = {
+               username: username,
+               comentario: comentario
+           };
+           // Añadir al array de comentarios
+           $scope.comentarios.push(comment);
+           // Limpiar inputs
+           $scope.username = '';
+           $scope.comentario = '';
+       }
+   };
 });
