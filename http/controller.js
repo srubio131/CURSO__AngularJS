98d537a3728e962abc108e57b6c8edf6b3e8ec58
Sergio R <s.rubio131@gmail.com>
2017-11-13 21:16:13 +0100

    Video 8. AJAX con $http (post)

--- http/controller.js
+++ http/controller.js
@@ -4,10 +4,29 @@ var app = angular.module("CursoAngularJS",[]);
 // Inyección de dependencias $scope, $http, etc
 app.controller("MainCtrl", ["$scope", "$http", function ($scope,$http) {
     $scope.posts = [];
+
     $http.get('https://jsonplaceholder.typicode.com/posts')
         .success(function (data) {
             $scope.posts = data;
         }).error(function (error) {
-            console.error("Error!! - ", error);
+            console.error("Error get!! - ", error);
         });
+
+    $scope.addPost = function (title, body) {
+        $http.post('https://jsonplaceholder.typicode.com/posts', {
+            title: title,
+            body: body,
+            userId: 1
+        }).success(function (data,status,headers,config) {
+            // Agregar el post al array
+            $scope.posts.push(data);
+            // Inicializar variables
+            $scope.newTitle = '';
+            $scope.newBody  = '';
+        })
+        .error(function (error,status,headers,config) {
+            console.error("Error post!! - ", error);
+        });
+    };
+
 }]);
