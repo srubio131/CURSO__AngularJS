7ee48542e0114538e32f4ac95bb04204d6b4d8f8
Sergio R <s.rubio131@gmail.com>
2017-11-15 18:23:03 +0100

    Video 9.TodoList

---
+++ todo/main.controller.js
@@ -0,0 +1,28 @@
+var app = angular.module("TodoList",["LocalStorageModule"]);
+
+app.controller("MainCtrl", ["$scope", "localStorageService", function ($scope, localStorageService) {
+
+    // Recuperar lista todos del storage
+    if (localStorageService.get("angular-todolist")) {
+        $scope.todos = localStorageService.get("angular-todolist");
+    } else {
+        $scope.todos = [];
+    }
+
+    // Observar cambios en nuewaTarea
+    $scope.$watchCollection('todos',function (newvalue,oldvalue) {
+        localStorageService.set("angular-todolist", $scope.todos);
+    });
+
+    // Añadir nueva tarea
+    $scope.addTarea = function(tarea) {
+        $scope.todos.push(tarea);
+        $scope.nuevaTarea = {}; // Limpiar
+    };
+
+    // Limpiar lista
+    $scope.clean = function () {
+        $scope.todos = [];
+    };
+
+}]);
