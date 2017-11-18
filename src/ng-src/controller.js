af96156959dc8d437f53c280665555458b82da92
Sergio R <s.rubio131@gmail.com>
2017-11-18 12:57:21 +0100

    Video 17. Ng-src y ng-cloack

--- src/ng-src/controller.js
+++ src/ng-src/controller.js
@@ -1,7 +1,18 @@
 // Identificador y array de módulos para registrarlos en la app (ngResource:consumir api rest)
-var app = angular.module("CursoAngularJS",[]);
+var app = angular.module("CursoAngularJS",['restangular']);
 
-// Controlador
-app.controller("MainCtrl", ["$scope", function ($scope) {
+// Constants
+app.constant("APIBASEURL","https://api.github.com/users/srubio131/");
 
+// Controladores
+app.controller("MainCtrl", ["$scope", "Restangular", "APIBASEURL",  function ($scope,Restangular,APIBASEURL) {
+
+    // Set baseUrl api github
+    Restangular.setBaseUrl(APIBASEURL);
+
+    Restangular.all('repos').getList().then(function(repos){
+        $scope.repos = repos;
+    }).catch(function (error) {
+        console.error("Error get!! - ", error);
+    });
 }]);
