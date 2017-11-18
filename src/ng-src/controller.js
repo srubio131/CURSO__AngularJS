4fffc3078620598dda00cf3455106cc59fe49bcd
Sergio R <s.rubio131@gmail.com>
2017-11-18 12:29:32 +0100

    Se ha cambiado el directorio de carpetas. Ahora todas las practicas estan dentro de 'src'

---
+++ src/ng-src/controller.js
@@ -0,0 +1,7 @@
+// Identificador y array de módulos para registrarlos en la app (ngResource:consumir api rest)
+var app = angular.module("CursoAngularJS",[]);
+
+// Controlador
+app.controller("MainCtrl", ["$scope", function ($scope) {
+
+}]);
